3aa2e6f563d332ece7c4ff3430149d7ec2c24a70
Anders Ekelund <anders.emil.ekelund@gmail.com>
2019-06-20 16:29:31 +0200

    Making sure to redirect to the proper Storyblok alternative story. (When using SB Multiple Tree i18n model)

--- components/SharedMixin.ts
+++ components/SharedMixin.ts
@@ -1,11 +1,13 @@
 import store from '@vue-storefront/core/store'
 import { isServer } from '@vue-storefront/core/helpers'
 import { mapGetters } from 'vuex'
-import { localizedRoute, removeStoreCodeFromRoute } from '@vue-storefront/core/lib/multistore'
+import { localizedRoute, removeStoreCodeFromRoute, storeCodeFromRoute } from '@vue-storefront/core/lib/multistore'
 import get from 'lodash.get'
+import StoryblokMixin from 'src/modules/vsf-storyblok-module/components/StoryblokMixin'
 
 export default {
   name: 'StorePickerMixin',
+  mixins: [StoryblokMixin],
   computed: {
     ...mapGetters({
       current: 'store-picker/currentStoreView',
@@ -22,6 +24,18 @@ export default {
       if (url === pathname) {
         url = '/'
       }
+
+      // Are we on a Storyblok CMS page?
+      if(this.story){
+        // Try to find an alternate story for the target storeview
+        let alternateStory = this.story.alternates.find((alternate) => {
+          return storeCodeFromRoute(alternate.full_slug) === view.storeCode
+        })
+        if(alternateStory){
+          url = '/' + removeStoreCodeFromRoute(alternateStory.full_slug)
+        }
+      }
+
       const route = localizedRoute(url, view.storeCode)
       return route.replace(/\/$/, "")
     },
