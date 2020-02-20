56ac68e19ac63fe5a09345b2bbebd3bcbb2abfff
Anders Ekelund <anders.emil.ekelund@gmail.com>
2020-02-20 18:14:17 +0100

    url now has leading slash in 1.11.x

--- forceStorecode.ts
+++ forceStorecode.ts
@@ -20,42 +20,36 @@ export const forceStorecode = async (context, { url }) => {
     if (storeViews.multistore && storeViews.forcePrefix && !storeCode) {
       const redirect = url => {
         const statusCode = storeViews.redirectStatusCode || 302
+        if (queryString) {
+          url = url + '?' + queryString
+        }
         res.set('location', url)
         res.status(statusCode).send()
       }
-      const createUrl = (_storeCode: string) => `/${_storeCode}/${url}`
+      const createUrl = (_storeCode: string) => `/${_storeCode}${url}`
+
       const cfCountry = req.headers.http_cf_ipcountry ? req.headers.http_cf_ipcountry.toLowerCase() : undefined
-      // cfCountry matches existing storeView code...
-      if (cfCountry && storeViews[cfCountry] && storeViews[cfCountry].disabled !== true) {
-        let newUrl = createUrl(cfCountry)
-        if (queryString) {
-          newUrl = newUrl + '?' + queryString
+      if (cfCountry) {
+        // cfCountry matches existing storeView code...
+        if (storeViews[cfCountry] && storeViews[cfCountry].disabled !== true) {
+          let newUrl = createUrl(cfCountry)
+          return redirect(newUrl)
         }
-        return redirect(newUrl)
-      }
-      // ...otherwise check if cfCountry is mapped to a specific storeView...
-      if (cfCountry && storeViews.countryStoreViewMapping && storeViews.countryStoreViewMapping[cfCountry]){
-        let newUrl = createUrl(storeViews.countryStoreViewMapping[cfCountry])
-        if (queryString) {
-          newUrl = newUrl + '?' + queryString
+        // ...otherwise check if cfCountry is mapped to a specific storeView...
+        if (storeViews.countryStoreViewMapping && storeViews.countryStoreViewMapping[cfCountry]){
+          let newUrl = createUrl(storeViews.countryStoreViewMapping[cfCountry])
+          return redirect(newUrl)
         }
-        return redirect(newUrl)
       }
       // ...otherwise check if a single fallback storeview is configured (ie our default storeView)
       if (storeViews.fallbackStoreCode) {
         let newUrl = createUrl(storeViews.fallbackStoreCode)
-        if (queryString) {
-          newUrl = newUrl + '?' + queryString
-        }
         return redirect(newUrl)
       }
       // ...we have nothing to go on. Just pick any available storeview.
       const lastResort: any = Object.values(storeViews)
         .find((view: any) => view && view.storeCode && !view.disabled)
       let newUrl = createUrl(lastResort.storeCode)
-      if (queryString) {
-        newUrl = newUrl + '?' + queryString
-      }
       return redirect(newUrl)
     }
   }
