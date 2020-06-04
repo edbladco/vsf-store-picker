12969b334767d6ac3afd9f84ad4f1f52db10a6bd
Andreas Karlsson <andreas.karlsson@indiebytes.se>
2020-06-04 21:28:51 +0200

    Re-arrange code to make req available and avoid TS2339

--- server.ts
+++ server.ts
@@ -6,8 +6,6 @@ if (appConfig.storeViews.multistore) {
     const blacklist = ['__webpack_hmr']
     const storeCodes = appConfig.storeViews.mapStoreUrlsFor
     const defaultStoreCode = appConfig.storeViews.fallbackStoreCode || storeCodes[0]
-    const cfCountry = req.headers.http_cf_ipcountry ? req.headers.http_cf_ipcountry.toLowerCase() : undefined
-
     const blacklistStr = blacklist.join('|')
     const storeCodesStr = storeCodes.join('|')
     const hasStoreCode = new RegExp(
@@ -16,19 +14,21 @@ if (appConfig.storeViews.multistore) {
 
     let storeCode = defaultStoreCode
 
-    if (cfCountry) {
-        // cfCountry matches existing storeView code...
-      if (appConfig.storeViews[cfCountry] && appConfig.storeViews[cfCountry].disabled !== true) {
-        storeCode = cfCountry
-      }
-      // ... otherwise check if cfCountry is mapped to a specific storeView...
-      if (appConfig.storeViews.countryStoreViewMapping && appConfig.storeViews.countryStoreViewMapping[cfCountry]) {
-        storeCode = appConfig.storeViews.countryStoreViewMapping[cfCountry]
-      }
-    }
-
     app.get(hasStoreCode, (req, res) => {
       const { path } = req
+      const cfIpCountry = <string>req.headers.http_cf_ipcountry
+      const cfCountry = cfIpCountry ? cfIpCountry.toLowerCase() : undefined
+
+      if (cfCountry) {
+        // cfCountry matches existing storeView code...
+        if (appConfig.storeViews[cfCountry] && appConfig.storeViews[cfCountry].disabled !== true) {
+          storeCode = cfCountry
+        }
+        // ... otherwise check if cfCountry is mapped to a specific storeView...
+        if (appConfig.storeViews.countryStoreViewMapping && appConfig.storeViews.countryStoreViewMapping[cfCountry]) {
+          storeCode = appConfig.storeViews.countryStoreViewMapping[cfCountry]
+        }
+      }
       const newUrl = `/${storeCode}${path}`
 
       let query = ''
