c977a55ab53d8c8572104dcf69721944fa35938e
Andreas Karlsson <andreas.karlsson@indiebytes.se>
2020-06-04 21:06:05 +0200

    Add server hook to redirect to store Based on https://github.com/DivanteLtd/vue-storefront/issues/3399#issuecomment-565942028 and forceStorecode.ts.

---
+++ server.ts
@@ -0,0 +1,45 @@
+import appConfig from 'config'
+import { serverHooks } from '@vue-storefront/core/server/hooks'
+
+if (appConfig.storeViews.multistore) {
+  serverHooks.afterApplicationInitialized(({ app }) => {
+    const blacklist = ['__webpack_hmr']
+    const storeCodes = appConfig.storeViews.mapStoreUrlsFor
+    const defaultStoreCode = appConfig.storeViews.fallbackStoreCode || storeCodes[0]
+    const cfCountry = req.headers.http_cf_ipcountry ? req.headers.http_cf_ipcountry.toLowerCase() : undefined
+
+    const blacklistStr = blacklist.join('|')
+    const storeCodesStr = storeCodes.join('|')
+    const hasStoreCode = new RegExp(
+      `^((?!\\/(${storeCodesStr})(\\/|$))(?!\\/(${blacklistStr})$))\\/?.*((?<=\\.html)|(?<!\\.[a-zA-Z0-9]*))$`
+    )
+
+    let storeCode = defaultStoreCode
+
+    if (cfCountry) {
+        // cfCountry matches existing storeView code...
+      if (appConfig.storeViews[cfCountry] && appConfig.storeViews[cfCountry].disabled !== true) {
+        storeCode = cfCountry
+      }
+      // ... otherwise check if cfCountry is mapped to a specific storeView...
+      if (appConfig.storeViews.countryStoreViewMapping && appConfig.storeViews.countryStoreViewMapping[cfCountry]) {
+        storeCode = appConfig.storeViews.countryStoreViewMapping[cfCountry]
+      }
+    }
+
+    app.get(hasStoreCode, (req, res) => {
+      const { path } = req
+      const newUrl = `/${storeCode}${path}`
+
+      let query = ''
+      if (Object.values(req.query).length > 0) {
+        const params = new URLSearchParams(req.query)
+        query += '?' + params.toString()
+      }
+
+      res.redirect(newUrl + query)
+
+      console.log('Redirect to default:', newUrl)
+    })
+  })
+}
